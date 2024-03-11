a7e7bcdb4174c078ae5953c1eeaba1c23ef48a1b
vivek <vikumar@datopic.com>
2024-03-11 13:04:11 +0530

    setup the flow

--- backend/main.py
+++ backend/main.py
@@ -3,17 +3,6 @@ from core.config import settings
 from db.session import engine
 from db.base_class import Base
 
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-
-
-def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    create_tables()
-    return app
-
-
 app = FastAPI(title="Blog", version='0.1.0')
 
 @app.get("/")
